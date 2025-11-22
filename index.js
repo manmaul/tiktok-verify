418f364f0db6672bc891376321e7131fd6712e15
manmaul <proyectosmanmaul@gmail.com>
2025-11-22 12:57:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//index
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("Funny Scraper Running OK ✔");
+});
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor iniciado en puerto", PORT);
+});
